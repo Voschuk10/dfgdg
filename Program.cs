8a1e4739b173f75b6478cbb662f4bd747d479ba1
Voschuk10 <106572427+Voschuk10@users.noreply.github.com>
2022-10-06 13:11:46 +0300

    progect init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ооп1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
